4567ecbd67c79f2b8c0b15c96b0e25461e7e958c
Marko <marko3blazev@gmail.com>
2026-05-07 11:26:31 +0200

    commit

---
+++ src/Content.js
@@ -0,0 +1,9 @@
+import './Content.css';
+function Content(){
+    return(
+        <main className='Content'>
+            <h1>Početna stranica</h1>
+        </main>
+    )
+}
+export default Content;
