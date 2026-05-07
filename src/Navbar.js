7d5d0287a21ed587c466f0e0d8798ade4c27dbe2
Marko <marko3blazev@gmail.com>
2026-05-07 10:27:28 +0200

    commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <header className='Navbar'>
+            <h1>Azilija</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    )
+}
