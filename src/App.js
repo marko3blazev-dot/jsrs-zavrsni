5bc7a0600789d895939d04aa568f49f432c0e508
Marko <marko3blazev@gmail.com>
2026-05-07 19:45:10 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Content from './Content';
 import Footer from "./Footer";
+import Donacije from "./Components/Donacije";
 function App() {
   return (
     <div className="App">
