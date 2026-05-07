56911d2e4af99170b7f9fa327d663b770c6aa848
Marko <marko3blazev@gmail.com>
2026-05-07 12:17:22 +0200

    commit

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,19 @@
 import './Content.css';
-function Content(){
-    return(
+function Content() {
+    return (
         <main className='Content'>
             <h1>Početna stranica</h1>
+            <section className="hero-section">
+                <div className="hero-content">
+                    <h1 className="main-title">Mjesto novih početaka</h1>
+                    <p className="intro-text">
+                        Svaka životinja zaslužuje dom pun ljubavi i sigurnosti.
+                        U Aziliji svakodnevno radimo na tome da napuštenim, zaboravljenim i ranjenim
+                        šapicama pružimo toplinu, punu zdjelicu i, ono najvažnije, priliku za sretan život.
+                        Pregledaj naše štićenike – možda se tvoj novi najbolji prijatelj krije baš ovdje.
+                    </p>
+                </div>
+            </section>
         </main>
     )
 }
