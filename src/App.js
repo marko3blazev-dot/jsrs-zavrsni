7a4a253a247e9583ed0b56d42c891f51294da29a
Marko <marko3blazev@gmail.com>
2026-05-08 12:01:02 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -9,20 +9,17 @@ import Nasilje from "./Components/Nasilje";
 import Volontiranje from "./Components/Volontiranje";
 function App() {
   const psi = [
-    // ŠTENCI (< 1 god): indeks 0, 1, 2
-    { ime: "Bobi", vrsta: "Mješanac", kilaza: "8 kg", dob: "6 mjeseci" },   // Mali
-    { ime: "Lili", vrsta: "Pudla", kilaza: "15 kg", dob: "8 mjeseci" },    // Srednji
-    { ime: "Aron", vrsta: "Njemački ovčar", kilaza: "30 kg", dob: "10 mjeseci" }, // Veliki
+    { ime: "Bobi", vrsta: "Mješanac", kilaza: "8 kg", dob: "6 mjeseci" },
+    { ime: "Lili", vrsta: "Pudla", kilaza: "15 kg", dob: "8 mjeseci" },
+    { ime: "Aron", vrsta: "Njemački ovčar", kilaza: "30 kg", dob: "10 mjeseci" },
 
-    // ODRASLI (1 - 7 god): indeks 3, 4, 5
-    { ime: "Rex", vrsta: "Terijer", kilaza: "7 kg", dob: "3 godine" },     // Mali
-    { ime: "Luna", vrsta: "Zlatni retriver", kilaza: "22 kg", dob: "4 godine" }, // Srednji
-    { ime: "Vuk", vrsta: "Haski", kilaza: "35 kg", dob: "5 godina" },      // Veliki
+    { ime: "Rex", vrsta: "Terijer", kilaza: "7 kg", dob: "3 godine" },
+    { ime: "Luna", vrsta: "Zlatni retriver", kilaza: "22 kg", dob: "4 godine" },
+    { ime: "Vuk", vrsta: "Haski", kilaza: "35 kg", dob: "5 godina" },
 
-    // SENIORI (8+ god): indeks 6, 7, 8
-    { ime: "Kiki", vrsta: "Chihuahua", kilaza: "4 kg", dob: "10 godina" },  // Mali
-    { ime: "Žan", vrsta: "Španijel", kilaza: "18 kg", dob: "11 godina", link: "https://www.facebook.com/share/p/17NJ8ZqCQz/" },  // Srednji
-    { ime: "Medo", vrsta: "Bernardinac", kilaza: "45 kg", dob: "9 godina" }   // Veliki
+    { ime: "Kiki", vrsta: "Chihuahua", kilaza: "4 kg", dob: "10 godina" },
+    { ime: "Žan", vrsta: "Španijel", kilaza: "18 kg", dob: "11 godina", link: "https://www.facebook.com/share/p/17NJ8ZqCQz/" }, 
+    { ime: "Medo", vrsta: "Bernardinac", kilaza: "45 kg", dob: "9 godina" }
   ];
   return (
     <div className="App">
