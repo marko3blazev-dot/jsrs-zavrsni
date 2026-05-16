671d4cfaf5e6dc4f64973c059c8bbc5873045abd
Marko <marko3blazev@gmail.com>
2026-05-16 13:11:42 +0200

    commit

--- src/Components/Volontiranje.js
+++ src/Components/Volontiranje.js
@@ -7,30 +7,40 @@ function Volontiranje() {
             opis: "Pridružite se našim vikend akcijama socijalizacije i šetnje. Svaki korak s njima znači puno.",
             link: "https://www.facebook.com/share/1GefJQ976R/",
             izvor: "Facebook",
-            grad: "Šibenik"
+            grad: "Šibenik",
+            zupanija: "Šibensko-Kninska županija:"
         },
         {
             naslov: "Pomoć u azilu",
             opis: "Trebamo ruke za pomoć pri hranjenju, čišćenju i uređenju okoliša našeg skloništa.",
             link: "https://www.facebook.com/share/14e6rcYuoT7/",
             izvor: "Facebook",
-            grad: "Dubrovnik"
+            grad: "Dubrovnik",
+            zupanija: "Dubrovačko-Neretvanska županija:"
         },
         {
             naslov: "Svakodnevni rad i briga u azilu",
             opis: "Pridruži nam se u svakodnevnim zadacima: od hranjenja i održavanja čistoće, do uređenja prostora u kojem naše šapice borave.",
             link: "https://www.noina-arka.hr/kako-mogu-pomoci/",
             izvor: "Noina Arka",
-            grad: "Dumovec"
+            grad: "Dumovec",
+            zupanija: "Grad Zagreb"
         },
         {
             naslov: "Svjetski standardi volontiranja",
             opis: "Saznajte više o tome kako funkcionira volontiranje na globalnoj razini putem Petfindera.",
             link: "https://www.petfinder.com/",
             izvor: "Petfinder",
-            grad: "Globalno"
+            grad: "Globalno",
+            zupanija: "Međunarodno"
         }
     ];
+    const prilikePoZupanijama = prilike.reduce((grupe, prilika) => {
+        const zupanija = prilika.zupanija || "Ostale županije";
+        if (!grupe[zupanija]) grupe[zupanija] = [];
+        grupe[zupanija].push(prilika);
+        return grupe;
+    }, {});
     return (
         <div id="volontiranje">
             <section className="v-info-sekcija">
@@ -64,19 +74,33 @@ function Volontiranje() {
 
             <section className="v-linkovi-sekcija">
                 <h2>Trenutne prilike za pomoć</h2>
-                <div className="v-grid-poveznica">
-                    {prilike.map((item, index) => (
-                        <a href={item.link} target="_blank" rel="noopener noreferrer" key={index} className="v-vanjski-link">
-                            
-                            <div className="kartica-tagovi">
-                                <span className="izvor-tag">{item.izvor}</span>
-                                <span className="grad-tag">📍 {item.grad}</span>
+                <div className="kategorije-stupac">
+
+                    {Object.keys(prilikePoZupanijama).sort().map((imeZupanije) => (
+
+                        <details className="dobna-skupina" key={imeZupanije}>
+                            <summary className="accordion-header">{imeZupanije}</summary>
+                            <div className="accordion-content">
+
+                                <div className="v-grid-poveznica">
+                                    {prilikePoZupanijama[imeZupanije].map((item, index) => (
+                                        <a href={item.link} target="_blank" rel="noopener noreferrer" key={index} className="v-vanjski-link">
+
+                                            <div className="kartica-tagovi">
+                                                <span className="izvor-tag">{item.izvor}</span>
+                                                <span className="grad-tag">📍 {item.grad}</span>
+                                            </div>
+
+                                            <h3>{item.naslov}</h3>
+                                            <p>{item.opis}</p>
+                                            <div className="v-strelica">Saznaj više &rarr;</div>
+                                        </a>
+                                    ))}
+                                </div>
+
                             </div>
-                            
-                            <h3>{item.naslov}</h3>
-                            <p>{item.opis}</p>
-                            <div className="v-strelica">Saznaj više &rarr;</div>
-                        </a>
+                        </details>
+
                     ))}
                 </div>
             </section>
